33c6e6947c4da8ebf1b904d97423b585c8d9a10e
holdingj <35545239+holdingj@users.noreply.github.com>
2018-03-11 23:39:51 -0600

    Add files via upload
    added code.

--- capsketch.ino
+++ capsketch.ino
@@ -17,6 +17,7 @@
 //D10       - Relay enable - 10000 ohm
 //D11       - Relay enable - 100000 ohm
 //D12       - Relay enable - 1 megaohm
+//D13       - Relay enable - supply to cap
 
 
 /*Hardware abstraction*/
@@ -47,7 +48,7 @@ class outPin
 class inPin
 {
   public:
-    int get(bool refresh, const int &time)
+    int get(bool refresh)
     {
       if (refresh) //only update reading when we ask for it, we don't want to spam read the discharching cap as it may affect the result
       {
@@ -69,11 +70,6 @@ class inPin
 };
 /* End hardware abstraction */
 
-
-
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
 inPin autoRange(A0);
 inPin rangedSupply(A1);
 inPin rangedCapSide(A2);
@@ -86,9 +82,14 @@ outPin relay_1kohm(9);
 outPin relay_10kohm(10);
 outPin relay_100kohm(11);
 outPin relay_1mohm(12);
+outPin relay_capSupply(13);
+
+int startTime = millis();
+
+void setup() {
+Serial.begin(115200);
 attachInterrupt(digitalPinToInterrupt(2), resetButton, FALLING);
 attachInterrupt(digitalPinToInterrupt(3), startButton, FALLING);
-int startTime = millis();
 }
 
 void loop() {
@@ -98,12 +99,77 @@ void loop() {
 
 void resetButton()
 {
+  relay_30v.set(LOW);
+  relay_15v.set(LOW);
+  relay_5v.set(LOW);
+  relay_1v.set(LOW);
+  relay_100ohm.set(LOW);
+  relay_1kohm.set(LOW);
+  relay_10kohm.set(LOW);
+  relay_100kohm.set(LOW);
+  relay_1mohm.set(LOW);
+  startTime = millis();
+
+  
   return;
 }
 
 void startButton()
 {
-  return;
+  int rangeValue = autoRange.get(true); //Learn what the power supply value is (30V = 1024)
+  int range = 0;
+
+  if (rangeValue > 512) // More than 15V
+  {
+    relay_30v.set(HIGH);
+    range = 1;
+  }
+  else if (rangeValue > 200) // More than 5V
+  {
+    relay_15v.set(HIGH);
+    range = 2;
+  }
+  else if (rangeValue > 33) // More than 1V
+  {
+    relay_5v.set(HIGH);
+    range = 3;
+  }
+  else if (rangeValue > 0) // More than grounded
+  {
+    relay_1v.set(HIGH);
+    range = 4;
+  }
+
+//At this point rangedSupply and rangedCapSide should be properly voltage limited
+
+
+  chargeCap(); 
+  startTime = millis();
+/*    relay_100ohm.set(LOW);
+  relay_1kohm.set(LOW);
+  relay_10kohm.set(LOW);
+  relay_100kohm.set(LOW);
+  relay_1mohm.set(LOW); */
+  
+  
+  
+
+}
+
+void chargeCap()
+{
+  //Detect when the cap is fully charged to supply voltage. This will check the voltage every 100 ms and continue
+  relay_capSupply.set(HIGH); //Connect cap to supply
+  
+  int delta = 1023;
+  int measValue = rangedCapSide.get(true);
+  while (delta > 0)
+  {
+    delay(100);
+    delta = rangedCapSide.get(true) - measValue;
+    measValue = rangedCapSide.get(false);
+  }
+  relay_capSupply.set(LOW);
 }
 
 
